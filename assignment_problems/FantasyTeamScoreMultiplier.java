c1d41000875d22e795d69805cb785c26443ec47b
Thaks07 <at6111@srmist.edu.in>
2026-09-21 12:44:04 +0530

    Add Category C assignment problems

---
+++ assignment_problems/FantasyTeamScoreMultiplier.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FantasyTeamScoreMultiplier {
+
+    static void applyMultipliers(double[] playerScores, int captainIndex, int viceCaptainIndex) {
+        playerScores[captainIndex] = playerScores[captainIndex] * 2;
+        playerScores[viceCaptainIndex] = playerScores[viceCaptainIndex] * 1.5;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] scores = {40, 55, 30, 62};
+
+        applyMultipliers(scores, 1, 3);
+
+        System.out.println(java.util.Arrays.toString(scores));
+
+        sc.close();
+    }
+}
